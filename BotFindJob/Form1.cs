7c0c58432b5a660ea8ccdab8d4b74d2472af8ac9
Viktor <svifer95@gmail.com>
2019-10-09 11:02:47 +0300

    Start bot
    Load first example

---
+++ BotFindJob/Form1.cs
@@ -0,0 +1,73 @@
+﻿using MihaZupan;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Telegram.Bot;
+
+namespace BotFindJob
+{
+	public partial class Form1 : Form
+	{
+		BackgroundWorker bw;
+		public Form1()
+		{
+			InitializeComponent();
+
+			this.bw = new BackgroundWorker();
+			this.bw.DoWork += this.bw_DoWork;
+		}
+		async void bw_DoWork(object sender, DoWorkEventArgs e)
+		{
+			var worker = sender as BackgroundWorker;
+			var key = e.Argument as string; // получаем ключ из аргументов
+			try
+			{
+				var proxy = new HttpToSocks5Proxy("207.180.238.12", 1080);
+				var Bot = new TelegramBotClient(key, proxy);
+				await Bot.SetWebhookAsync("");
+				int offset = 0;
+				while(true)
+				{
+					var updates = await Bot.GetUpdatesAsync(offset);//получаем массив обновлений
+					foreach(var update in updates)
+					{
+						//Console.WriteLine(update.Type);
+						var message = update.Message;
+						if(message.Type == Telegram.Bot.Types.Enums.MessageType.Text)
+						{
+							if(message.Text == "/saysomething")
+							{
+								await Bot.SendTextMessageAsync(message.Chat.Id, "test", replyToMessageId: message.MessageId);
+							}
+						}
+						offset = update.Id + 1;
+					}
+				}
+			}
+			catch(Telegram.Bot.Exceptions.ApiRequestException ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+		}
+
+		void BtnRunClick(object sender, EventArgs e)
+		{
+			var text = "765215433:AAEN9q1sxI6e4OWoDTxwzUFjBYoUN0xReqc";
+			if(!this.bw.IsBusy)//если не запущен
+			{
+				this.bw.RunWorkerAsync(text);//запускаем
+			}
+		}
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
